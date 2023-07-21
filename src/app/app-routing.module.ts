5bd1a1b54711bdacb08f5e5cb2a08706f93615bf
Emmanuel Londoño Gomez <emmalondogo@gmail.com>
2023-07-21 10:09:33 -0500

    Feat: Initialized project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { DetailComponent } from './pages/detail/detail.component';
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './pages/list/list.component';
+
+const routes: Routes = [
+  {path:'',component:ListComponent,},
+  {path:'card/:id', component:DetailComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
